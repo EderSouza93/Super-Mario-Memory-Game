921b88a5e7b0ebf91ee93b8d32aaf99bd31cb99d
EderSouza93 <84224107+EderSouza93@users.noreply.github.com>
2023-05-14 19:26:37 -0300

    Versão 0.6

--- js/script.js
+++ js/script.js
@@ -48,56 +48,83 @@ const cardsArray = [
         name: 'goomba',
         img: 'img/goomba.png',
     },
-]
-
-let count = 0;
+];
 
 // Duplicando array para criar uma correspondência para cada card
 let gameGrid = cardsArray.concat(cardsArray);
-
 // Randomizando a exibição de cartões
-gameGrid.sort(() => 0.5 - Math.random())
+gameGrid.sort(() => 0.5 - Math.random());
+
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let previousTarget = null;
 
 // Pegue o div com um id de root
 const game = document.querySelector('#game');
-
 // Cria uma seção com uma section grid
 const grid = document.createElement('section'); 
-grid.setAttribute('class', 'grid')
-
+grid.setAttribute('class', 'grid');
 // Anexa a section grid ao div game
-game.appendChild(grid)
+game.appendChild(grid);
 
 // For each item no array cardsArray...
 gameGrid.forEach((item) => {
     // criando uma div
-    const card = document.createElement('div')
-
+    const card = document.createElement('div');
     // Aplicando uma card class a essa div
-    card.classList.add('card')
-
+    card.classList.add('card');
     // Definindo o atributo data-name do div para o nome cardsArray
-    card.dataset.name = item.name
-
+    card.dataset.name = item.name;
     // Aplicando a imagem de fundo do div à imagem cardsArray
-    card.style.backgroundImage = `url(${item.img})`
-
+    card.style.backgroundImage = `url(${item.img})`;
     // Anexando o div ao grid section
-     grid.appendChild(card)
-})
+     grid.appendChild(card);
+});
+
+// Adicionando match CSS
+const match = () => {
+    var selected = document.querySelectorAll('.selected');
+    selected.forEach((card) => {
+        card.classList.add('match');
+    });
+}
 
 // Adicionando o evento listener à grid 
 grid.addEventListener('click', function (event){
     // O evento marcará o item clicado
-    let clicked = event.target
+    let clicked = event.target;
     // Não permite que a própria section da grid seja selecionada; seleciona apenas as divs dentro da grid
-    if (clicked.nodeName === 'SECTION'){ return; }
+    if (clicked.nodeName === 'SECTION'|| clicked === previousTarget) { 
+        return; 
+    }
+    // Não permite selecionar mais de dois itens 
     if (count < 2) {
-        count++
-           // Adiciona a class selecionada
-    clicked.classList.add('selected')
+        count++;
+        if (count === 1) {
+            // Atribui o primeiro palpite
+            firstGuess = clicked.dataset.name;
+            console.log(firstGuess);
+            clicked.classList.add('selected');
+        } else {
+            // Atribui o segundo palpite 
+            secondGuess = clicked.dataset.name;
+            console.log(secondGuess);
+            clicked.classList.add('selected');
+        }
+        // Se ambos palpites não estiverem vazios...
+        if (firstGuess !== '' && secondGuess !== '') {
+            // e o primeiro palpite corresponder ao segundo...
+            if (firstGuess === secondGuess){
+                // rode a função match
+                match();
+            }
+        }
+        // define o previous target como clicked
+        previousTarget = clicked;
+           
     }  
-})
+});
 
 
 
